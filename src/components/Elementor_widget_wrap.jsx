95378902de7bc0e6a6348a8da3f485886026c90d
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-07-14 20:52:20 +0530

    new

--- src/components/Elementor_widget_wrap.jsx
+++ src/components/Elementor_widget_wrap.jsx
@@ -4,22 +4,22 @@ import Link from 'next/link'
 function Elementor_widget_wrap(props) {
   return (
       <div className='elementor_widget_wrap d-flex justify-content-center p-1 mx-3 align-middle'>
-          <a href = "/Consulting">
+          <Link href = "/Consulting">
             <h4 className='text-light' style = {{"fontWeight":"600", "fontSize": "18px"}}>{props.heading1}</h4>
             <p className='text-light' style = {{"fontWeight":"400", "fontSize": "15px"}}>{props.about1}</p>
-          </a>
-          <a href = '/Accounting'>
+          </Link>
+          <Link href = '/Accounting'>
             <h4 className='text-light' style = {{"fontWeight":"600", "fontSize": "18px"}}>{props.heading2}</h4>
             <p className='text-light' style = {{"fontWeight":"400", "fontSize": "15px"}}>{props.about2}</p>
-          </a>
-          <a href = '/Assurance'>
+          </Link>
+          <Link href = '/Assurance'>
             <h4 className='text-light' style = {{"fontWeight":"600", "fontSize": "18px"}}>{props.heading3}</h4>
             <p className='text-light' style = {{"fontWeight":"400", "fontSize": "15px"}}>{props.about3}</p>
-          </a>
-          <a href = '/Tax' >
+          </Link>
+          <Link href = '/Tax' >
             <h4 className='text-light' style = {{"fontWeight":"600", "fontSize": "18px"}}>{props.heading4}</h4>
             <p className='text-light' style = {{"fontWeight":"400", "fontSize": "15px"}}>{props.about4}</p>
-          </a>
+          </Link>
       </div>
   )
 }
