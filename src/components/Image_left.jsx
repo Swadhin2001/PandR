9da145236a643fee8e41fcd00beb0c8a9c7e6929
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-07-18 14:21:59 +0530

    bug fix

--- src/components/Image_left.jsx
+++ src/components/Image_left.jsx
@@ -6,16 +6,10 @@ function Image_left(props) {
     <div className='container '>
         <div className="row g-0">
             <div className="col col-md-6" style = {{"backgroundImage": `url(${props.image})`, "backgroundPosition":"center center", "backgroundRepeat":"no-repeat", "backgroundSize": "cover", "height": "500px"}}>
-              {/* <Image
-                src = {props.image}
-                alt = ""
-                className='img-fluid'
-                style = {{"width": "100%"}}
-              /> */}
             </div>
-            <div className="col col-md-6 vertical-center p-4" style={{"width":"50%", "height":"100%"}}>
-              <div className='mx-4 p-0' style={{"width":"100%"}} >
-                <h4 style = {{"fontWeight": "600", "fontSize": "24px"}}>{props.heading}</h4>
+            <div className="col col-md-6" style={{"display": "flex","alignItems": "center"}}>
+              <div className='mx-4 p-2' style={{"width":"100%"}} >
+                <h4 style = {{"fontWeight": "600", "fontSize": "24px"}} className='pb-3'>{props.heading}</h4>
                 <p style = {{"fontWeight":"400", "fontSize": "15px"}} >{props.about}</p>
               </div>
             </div>
--- src/components/Image_right.jsx
+++ src/components/Image_right.jsx
@@ -6,20 +6,14 @@ function Image_left(props) {
   return (
     <div className='container'>
         <div className="row g-0">
-            <div className="col col-md-6 p-4" style={{"width":"50%", "height":"500px"}}>
-              <div className='mx-3 p-0 ' style={{"width":"100%", "height": "100%"}}>
-                <h4 style = {{"fontWeight":"600", "fontSize": "24px"}} >{props.heading}</h4>
-                <p style = {{"fontWeight":"400", "fontSize": "15px"}}>{props.about}</p>
+            <div className="col col-md-6 " style={{"display": "flex","alignItems": "center"}}>
+              <div className='mx-3 p-2 ' style={{}}>
+                <h4 style = {{"fontWeight":"600", "fontSize": "24px"}} className='pb-3' >{props.heading}</h4>
+                <p style = {{"fontWeight":"400", "fontSize": "15px"}} className='pb-2'>{props.about}</p>
                 <Link href={`${props.link}`} className='text-decoration-none text-dark'>{props.linkText}</Link>
               </div>
             </div>
             <div className="col col-md-6" style = {{"backgroundImage": `url(${props.image})`, "backgroundPosition":"center center", "backgroundRepeat":"no-repeat", "backgroundSize": "cover", "height": "500px"}}>
-              {/* <Image
-                src = {props.image}
-                alt = ""
-                className='img-fluid'
-                style = {{"width": "100%"}}
-              /> */}
             </div>
         </div>
     </div>
