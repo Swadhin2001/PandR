884865efebac916ebeb5180fc6208f957b096580
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-08-11 23:51:01 +0530

    bug fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,6 +17,7 @@ export default function App({ Component, pageProps }) {
           href="https://cdn.jsdelivr.net/npm/segoe-fonts@1.0.1/segoe-fonts.min.css"
           rel="stylesheet"
         />
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
       </Head>
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
