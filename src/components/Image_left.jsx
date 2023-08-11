526a8e523d5b16083c4f19f3ac6b85355809bf13
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-08-11 22:38:50 +0530

    bug fixed

--- src/components/Image_left.jsx
+++ src/components/Image_left.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import Link from 'next/link'
 
 function Image_left(props) {
   return (
     <>
       <div className='container-xl'>
-          <div className="row " >
+          <div className="row" >
               <div className="col-md-6 body-image-left" style = {{"backgroundImage": `url(${props.image.src})`}}>
               </div>
               <div className="col-md-6 body-left-image-about about-mobile-space" >
-                <div className='mx-4 p-2'>
+                <div className='image-about'>
                   <h4  className='pb-3 body-image-h4' >{props.heading}</h4>
                   <p  className='pb-2 body-image-p' >{props.about}</p>
-                  <Link href={`${props.link}`} className='text-decoration-none text-dark'>{props.linkText}</Link>
                 </div>
               </div>
           </div>
--- src/components/Image_right.jsx
+++ src/components/Image_right.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import Link from 'next/link'
 
 function Image_left(props) {
   return (
     <div className='container-xl'>
         <div className="row row-odd">
             <div className="col-md-6 body-right-image-about about-mobile-space" >
-              <div className='mx-3 p-2'>
+              <div className='image-about'>
                 <h4 className='pb-3 body-image-h4' >{props.heading}</h4>
                 <p className='pb-2 body-image-p'>{props.about}</p>
-                <Link href={`${props.link}`} className='text-decoration-none text-dark'>{props.linkText}</Link>
               </div>
             </div>
             <div className="col-md-6 body-image-right" style = {{"backgroundImage": `url(${props.image.src})`}}>
