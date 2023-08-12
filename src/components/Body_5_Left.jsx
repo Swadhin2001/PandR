abf74a37f65543211d26b011e74160775163a399
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-08-12 10:52:09 +0530

    bug fixed

--- src/components/Body_5_Left.jsx
+++ src/components/Body_5_Left.jsx
@@ -5,27 +5,30 @@ import Image_right from './Image_right'
 
 function Body_5_Left(props) {
   return (
-    <div className='mainBody'>
-      <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
-  transition={{ duration: 1}}>
-          <Image_left image = {props.image2} heading = {props.heading2} about = {props.about2}/>
-        </motion.div>
+    <div className="d-flex justify-content-center">
+
+      <div className='mainBody'>
         <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
-  transition={{ duration: 1}}>
-          <Image_right ight image = {props.image3} heading = {props.heading3} about = {props.about3}/>
-        </motion.div>
-        <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
-  transition={{ duration: 1}}>
-          <Image_left image = {props.image4} heading = {props.heading4} about = {props.about4}/>
-        </motion.div>
-        <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
-  transition={{ duration: 1}}>
-          <Image_right image = {props.image5} heading = {props.heading5} about = {props.about5}/>
-        </motion.div>
-        <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
-  transition={{ duration: 1}}>
-          <Image_left image = {props.image6} heading = {props.heading6} about = {props.about6}/>
-        </motion.div>
+    transition={{ duration: 1}}>
+            <Image_left image = {props.image2} heading = {props.heading2} about = {props.about2}/>
+          </motion.div>
+          <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
+    transition={{ duration: 1}}>
+            <Image_right ight image = {props.image3} heading = {props.heading3} about = {props.about3}/>
+          </motion.div>
+          <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
+    transition={{ duration: 1}}>
+            <Image_left image = {props.image4} heading = {props.heading4} about = {props.about4}/>
+          </motion.div>
+          <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
+    transition={{ duration: 1}}>
+            <Image_right image = {props.image5} heading = {props.heading5} about = {props.about5}/>
+          </motion.div>
+          <motion.div initial = {{opacity: 0, y: -20}} whileInView={{opacity: 1, y : 0}} 
+    transition={{ duration: 1}}>
+            <Image_left image = {props.image6} heading = {props.heading6} about = {props.about6}/>
+          </motion.div>
+      </div>
     </div>
   )
 }
