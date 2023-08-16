12583e2edb02ecb407afd37a26585bc301ca1c96
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-08-16 21:45:32 +0530

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
 
   return (
     <>
-      <div className="d-flex justify-content-center custom-nav-class">
+      {/* <div className="d-flex justify-content-center custom-nav-class">
         <div className="container-1200">
           <nav className="navbar navbar-expand-lg " style= {{"animation": "transitionIn 2s"}}>
             <div className="container-fluid p-0 ">
@@ -16,8 +16,8 @@ function Navbar() {
                 <Image
                   src = {logo}
                   alt = " "
-                  style = {{"width": "41%", "height": "20%"}}
-                  className='p-0'
+                  // style = {{"width": "41%", "height": "20%"}}
+                  className='p-0 logo-class'
                 />
               </Link>
               <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -53,7 +53,54 @@ function Navbar() {
             </div>
           </nav>
         </div>
-      </div>
+      </div> */}
+
+      <nav className="navbar navbar-expand-lg custom-nav-class">
+        <div className="container-fluid ">
+          <Link className="navbar-brand" href = "/">
+            <Image
+              src={logo}
+              alt=" "
+              className='p-0 logo-class'
+            />
+          </Link>
+          <div className="d-flex" >
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item nav-padding">
+                  <Link className="nav-link active text-white p-0 nav-item-menu" aria-current="page" href='/about-us' style={{ "fontWeight": "600", "fontSize": "16px" }}>About Us</Link>
+                </li>
+                <li className="nav-item nav-padding">
+                  <Link className="nav-link text-white p-0" href="/our-team" style={{ "fontWeight": "600", "fontSize": "16px" }}>Our Team</Link>
+                </li>
+
+                <li className="nav-item dropdown nav-padding">
+                  <a className="nav-link dropdown-toggle p-0 text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ "fontWeight": "600", "fontSize": "16px" }}>
+                    Services
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><Link className="dropdown-item text-dark" href='/consulting' >Consulting</Link></li>
+                    <li><Link className="dropdown-item text-dark" href='/accounting'  >Accounting</Link></li>
+                    <li><Link className="dropdown-item text-dark" href='/assurance' >Assurance</Link></li>
+                    <li><Link className="dropdown-item text-dark" href='/tax' >Tax</Link></li>
+                  </ul>
+                </li>
+                <li className="nav-item nav-padding">
+                  <Link className="nav-link text-white p-0" href="/work-with-us" style={{ "fontWeight": "600", "fontSize": "16px" }}>Work With Us</Link>
+                </li>
+                <li className="nav-item nav-padding">
+                  <Link className="nav-link text-white p-0" href="/work-with-us" style={{ "fontWeight": "600", "fontSize": "16px" }}>Contact Us</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+
     </>
   )
 }
