c8a97a8998c260661e5a8f0de38737dc050bcfbc
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-08-18 16:28:44 +0530

    image changed

--- src/components/Our_current_open_positions.jsx
+++ src/components/Our_current_open_positions.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import image1 from '@/assets/Integrity-and-trust-matters-in-our-practice.jpg'
+import image2 from '@/assets/Senior-Executive-Audit-&-Tax-3-Positions.jpeg'
 import { motion } from 'framer-motion'
 
 
@@ -18,7 +19,7 @@ function Our_current_open_positions() {
                     <div className="row my-5">
                         <div className="col-md-6">
                             <div className='d-flex border border-secondary our-curr-pos'>
-                                <Image src={image1} alt="" style={{ "width": "170px", "height": "242px", "objectFit": "cover" }} />
+                                <Image src={image1} alt="" className='our-curr-pos-img'/>
                                 <div style={{ "padding": "15px 0px 0px 15px" }}>
                                     <h4 className='current-pos-h4 pb-3 m-0'>Manager Audit & Tax: 1 Position</h4>
                                     <p className='current-pos-p pe-1 m-0'>Qualified Chartered Accountant with 2+ years post qualification experience in CA firm. The Manager would be responsible to plan & execute agreed professional client engagements independently, with support with team of executives & articles. In addition to the client servicing, the role also requires the Manager to provide guidance & mentorship to the team.</p>
@@ -27,7 +28,7 @@ function Our_current_open_positions() {
                         </div>
                         <div className="col-md-6">
                             <div className='d-flex border border-secondary our-curr-pos'>
-                                <Image src={image1} alt="" style={{ "width": "170px", "height": "242px", "objectFit": "cover" }} />
+                                <Image src={image2} alt=""  className='our-curr-pos-img'/>
                                 <div style={{ "padding": "15px 0px 0px 15px" }}>
                                     <h4 className='current-pos-h4 pb-3 m-0'>Senior Executive Audit & Tax- 3 Positions</h4>
                                     <p className='current-pos-p pe-1 m-0'>Graduate with degree in commerce, semi-qualified with experience of at least 3+ years, with ability to independently handle the audits & related engagements in consulting, audit, and tax matters for subsidiaries of global clients or domestic clients.</p>
