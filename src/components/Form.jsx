a2acd836ca5d587a7d3a6b87256b8b38efff227d
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-10-18 14:53:25 +0530

    form updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Link from 'next/link'
+import emailjs from '@emailjs/browser';
+
+
 function Form() {
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
     return (
         <div>
-            <form>
+            <form ref= {form} onSubmit={sendEmail}>
                 <h4 className='form-h4'>Please fill the below form for your assistance</h4>
                 <div className="mb-1">
-                    <input type="text" id="TextInput" className="form-control rounded-0 form-focus shadow-none" placeholder="Name*" style={{"backgroundColor" : "#F9F9F9", "border": '0' , "color": "#2B2B2B"}} required />
+                    <input type="text" id="TextInput" className="form-control rounded-0 form-focus shadow-none" placeholder="Name*" style={{"backgroundColor" : "#F9F9F9", "border": '0' , "color": "#2B2B2B"}} required name = "form-name" />
                 </div>
                 <div className="mb-1">
-                    <input type="email" id="TextInput" className="form-control rounded-0 form-focus shadow-none" placeholder="Business Email*" style={{"backgroundColor" : "#F9F9F9", "border": '0' , "color": "#2B2B2B"}} required/>
+                    <input type="email" id="TextInput" className="form-control rounded-0 form-focus shadow-none" placeholder="Business Email*" style={{"backgroundColor" : "#F9F9F9", "border": '0' , "color": "#2B2B2B"}} required name = "form-email"/>
                 </div>
                 <div className="mb-1">
-                    <input type="text" id="TextInput" className="form-control rounded-0 form-focus shadow-none" placeholder="Phone Number*" style={{"backgroundColor" : "#F9F9F9", "border": '0' , "color": "#2B2B2B"}} required/>
+                    <input type="text" id="TextInput" className="form-control rounded-0 form-focus shadow-none" placeholder="Phone Number*" style={{"backgroundColor" : "#F9F9F9", "border": '0' , "color": "#2B2B2B"}} required name = "form-phonenumber"/>
                 </div>
                 <div className="mb-3">
-                    <textarea className="form-control form-focus shadow-none" placeholder="How can he help?*" id="floatingTextarea" style={{"backgroundColor" : "#F9F9F9", "border": '0', "color": "#2B2B2B"}}></textarea>
+                    <textarea className="form-control form-focus shadow-none" placeholder="How can he help?*" id="floatingTextarea" style={{"backgroundColor" : "#F9F9F9", "border": '0', "color": "#2B2B2B"}} name = "form-help"></textarea>
                 </div>
                 <div className="mb-3">
                     <p className='form-p form-focus shadow-none'>We care about your privacy. By submitting this form, you will receive the requested information, as well as occasional related business insights from PandR. You can unsubscribe at any time. For details, view our  
                     <Link href = "/policy" className='text-decoration-none'> Privacy Policy</Link>.</p>
                 </div>
-                <button type="submit" className="btn btn-dark rounded-0">Get in Touch</button>
+                <input type="submit" className="btn btn-dark rounded-0" value="Get in Touch"/>
             </form>
         </div>
     )
